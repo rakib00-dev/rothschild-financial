3f9d492742a1f3d6a22cbe70a19f42d7b7043d6d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-05 00:42:31 +0600

    added the hero img

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section>
+      <img
+        src="/images/hero/rothschildandco-london-buildings-38-hr.jpg"
+        alt=""
+      />
+    </section>
+  );
+};
+
+export default Hero;
