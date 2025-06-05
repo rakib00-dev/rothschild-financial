325987caacbfc60c9de5ba8d0213c0fcb40cce29
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-05 08:39:01 +0600

    added worksText text

--- src/components/custom/WorksText.tsx
+++ src/components/custom/WorksText.tsx
@@ -1,5 +1,13 @@
 const WorksText = () => {
-  return <section className="max-w-7xl w-full mx-auto ">WorksText</section>;
+  return (
+    <section className="max-w-7xl w-full mx-auto ">
+      <h1>
+        With a team of 4,600 talented financial services specialists on the
+        ground in over 40 countries across the world, we deliver a unique global
+        perspective across four market-leading business divisions.
+      </h1>
+    </section>
+  );
 };
 
 export default WorksText;
