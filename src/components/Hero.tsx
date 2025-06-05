23b662ba7cbec51e289f625b4a8761063831529d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-05 08:30:52 +0600

    added shadcn UI to the project and fixed the hero section problem

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,23 +3,25 @@ const Hero = () => {
     angledBox: {
       backgroundColor: '#eee7e2',
       color: '#0b3055',
-      padding: '2rem',
-      maxWidth: '400px',
       clipPath: 'polygon(0 0, 100% 0, 95% 100%, 0% 100%)',
       boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
-      fontSize: '1.1rem',
     },
   };
   return (
     <section className="relative mt-0 w-full max-w-7xl mx-auto">
-      <div className="relative">
-        <img
-          src="/images/hero/rothschildandco-london-buildings-38-hr.jpg"
-          alt="rothschildandco-london-buildings-38-hr.jpg"
-          className="md:px-10 px-3 top-0"
-        />
-        <div className="absolute bottom-0">
-          <h1 className="angled-box" style={style.angledBox}>
+      <div
+        className="relative h-full"
+        style={{
+          background:
+            'url(/images/hero/rothschildandco-london-buildings-38-hr.jpg) 50% / cover',
+          height: '400px',
+        }}
+      >
+        <div className="absolute -bottom-10 left-5 md:left-15">
+          <h1
+            className="angled-box text-lg w-4/5 max-w-xs p-4 md:text-4xl md:max-w-lg md:p-8"
+            style={style.angledBox}
+          >
             One of the world's largest independent financial services groups
           </h1>
         </div>
