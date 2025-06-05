2eb6ae8a6a33d30cf8bcd4e7befd07143df3499d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-05 18:09:44 +0600

    fixed the link

--- src/components/custom/FourComplementary.tsx
+++ src/components/custom/FourComplementary.tsx
@@ -4,21 +4,25 @@ const FourComplementary = () => {
       title: 'Global Advisory',
       p: 'An impartial and insightful perspective on M&A, strategy and financing advisory to help our clients formulate and achieve their strategic goals.',
       src: '/images/four-comple/rothschildandco-london-people-86.jpg',
+      link: 'https://www.rothschildandco.com/en/global-advisory/',
     },
     {
       title: 'Wealth Management',
       p: 'A comprehensive range of Wealth Management services available across the globe, with a long-term approach that sets us apart from our peers.',
       src: '/images/four-comple/rothschildandco-paris-people-17.jpg',
+      link: 'https://www.rothschildandco.com/en/wealth-management/',
     },
     {
       title: 'Asset Management',
       p: 'Innovative, bespoke investment solutions designed around the needs of each and every client.',
       src: '/images/four-comple/rothschildandco-paris-people-59.jpg',
+      link: 'https://www.rothschildandco.com/en/asset-management/',
     },
     {
       title: 'Five Arrows',
       p: `The alternative assets arm of Rothschild & Co, deploying the firm's capital alongside that of a select set of leading institutional and private investors.`,
       src: '/images/four-comple/rothschildandco-london-people-97-rt.jpg',
+      link: 'https://www.rothschildandco.com/en/five-arrows/',
     },
   ];
 
@@ -40,9 +44,11 @@ const FourComplementary = () => {
                 />
               </div>
               <div className="mb-20 mx-4 md:left-4 top-[90%] absolute grid gap-5 max-w-sm p-8 bg-[var(--primary-color)] z-10">
-                <h2 className="text-2xl text-[var(--promoblock-text-heading)]">
-                  {e.title}
-                </h2>
+                <a id="fourLink" href={e.link}>
+                  <h2 className="text-2xl text-[var(--promoblock-text-heading)]">
+                    {e.title}
+                  </h2>
+                </a>
                 <p className="text-sm">{e.p}</p>
               </div>
             </div>
