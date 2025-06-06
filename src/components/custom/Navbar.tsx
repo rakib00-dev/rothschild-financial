cf7de807f332c3f3460f005d2d3dfaf765aadccc
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-06 07:24:19 +0600

    updated the navbar

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -27,38 +27,59 @@ const Navbar = () => {
 
   // window onLoad Entry
   const navBarFix = useRef<HTMLDivElement>(null);
-  const header_section = useRef(null);
 
   const [isNavMenuOpen, setIsNavMenuOpen] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('load', () => {
-      if (navBarFix.current && header_section.current) {
-        navBarFix.current.style.top = '0';
-      }
-    });
+    if (navBarFix.current) {
+      navBarFix.current.style.top = '0';
+    }
 
-    // header NavBar onScroll
-    window.addEventListener('scroll', () => {
+    const handleScroll = () => {
       const scrollValue = window.scrollY;
-      if (scrollValue >= 82) {
-        if (navBarFix.current) {
-          navBarFix.current.style.top = '0';
-          navBarFix.current.style.position = 'fixed';
-        }
-      } else {
-        if (navBarFix.current) {
-          navBarFix.current.style.top = '0';
-          navBarFix.current.style.position = 'relative';
-        }
+      if (navBarFix.current) {
+        navBarFix.current.style.top = '0';
+        navBarFix.current.style.position =
+          scrollValue >= 82 ? 'fixed' : 'relative';
       }
-    });
+    };
 
-    console.log(navBarFix);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
     <>
+      {' '}
+      <div
+        className={`transition-all grid place-items-start px-4 md:px-20 duration-300 fixed h-svh w-full bg-[var(--menu-bg)] text-white pt-25 z-30 ${
+          isNavMenuOpen ? 'top-0' : '-top-[100%]'
+        }`}
+      >
+        <div
+          className={`cursor-pointer absolute left-6 md:left-12 top-6 md:top-8 z-40 ${
+            !isNavMenuOpen ? `hidden` : 'block text-[var(--footer-link)]'
+          }`}
+          onClick={() => {
+            setIsNavMenuOpen(false);
+          }}
+        >
+          <RiCloseFill className="text-3xl" />
+        </div>
+        {menus.map((e) => (
+          <a
+            href="#"
+            className="transition-all duration-300 flex justify-between gap-10 items-center w-[400px] md:text-2xl menuLinkHover"
+            key={e}
+          >
+            {e}
+            <PiGreaterThanBold className="h-4 w-4" />
+          </a>
+        ))}
+      </div>
       <motion.div
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
@@ -74,27 +95,28 @@ const Navbar = () => {
           id="nav"
           className="relative flex justify-center items-center px-6 md:px-12 py-4 max-w-7xl border-b-1 border-gray-200 bg-white/60 p-4 w-full h-20 m-auto lg:py-0.5"
         >
-          <div
-            className={`cursor-pointer relative z-40 ${
-              !isNavMenuOpen ? `block` : 'hidden'
-            }`}
-            onClick={() => {
-              setIsNavMenuOpen((prev) => !prev);
-            }}
-          >
-            <RiMenu3Fill className="text-3xl rotate-180" />
+          <div className="aboslute z-40 left-0">
+            <div
+              className={`cursor-pointer relative z-40 ${
+                !isNavMenuOpen ? `block` : 'hidden'
+              }`}
+              onClick={() => {
+                setIsNavMenuOpen((prev) => !prev);
+              }}
+            >
+              <RiMenu3Fill className="text-3xl rotate-180" />
+            </div>
+            <div
+              className={`cursor-pointer relative z-40 ${
+                !isNavMenuOpen ? `hidden` : 'block text-[var(--footer-link)]'
+              }`}
+              onClick={() => {
+                setIsNavMenuOpen(false);
+              }}
+            >
+              <RiCloseFill className="text-3xl" />
+            </div>
           </div>
-          <div
-            className={`cursor-pointer relative z-40 ${
-              !isNavMenuOpen ? `hidden` : 'block text-[var(--footer-link)]'
-            }`}
-            onClick={() => {
-              setIsNavMenuOpen(false);
-            }}
-          >
-            <RiCloseFill className="text-3xl" />
-          </div>
-
           <div id="menu" className=" justify-center items-center w-full flex">
             <div
               id="logo"
@@ -125,22 +147,6 @@ const Navbar = () => {
           </div>
         </nav>
       </motion.div>
-
-      <div
-        className={`transition-all grid place-items-start px-4 md:px-20 duration-300 fixed h-svh w-full bg-[var(--menu-bg)] text-white pt-25 z-10 ${
-          isNavMenuOpen ? 'top-0' : '-top-[100%]'
-        }`}
-      >
-        {menus.map((e) => (
-          <a
-            href=""
-            className="transition-all duration-300 flex justify-between gap-10 items-center w-[400px] md:text-2xl menuLinkHover"
-          >
-            {e}
-            <PiGreaterThanBold className="h-4 w-4" />
-          </a>
-        ))}
-      </div>
     </>
   );
 };
